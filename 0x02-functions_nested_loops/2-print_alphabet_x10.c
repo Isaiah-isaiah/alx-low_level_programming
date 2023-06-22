30dd4ddb7dec91e9d76b95b492aebac288319ad2
Isaia-isaiah <isaiahipaye2@gmail.com>
2023-06-21 22:07:29 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+  
+/**
+* print_alphabet - Prints the alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_10(void)
+{
+          int i;
+	  int j;
+          for (j = 1; i <= 10; i++)
+          {
+		  for (j = 1; j <= 10; i++)
+		  {
+			  _putchar(j);
+		  }
+		  _putchar('\n');
+          }
+}
