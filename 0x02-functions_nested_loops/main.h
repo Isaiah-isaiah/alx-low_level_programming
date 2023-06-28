227b970106e22b1b483b741724ff61e055223036
Isaia-isaiah <isaiahipaye2@gmail.com>
2023-06-28 05:09:41 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,3 @@
-/**
-*File:main.h
-*Author:Pericles Adjovi
-*Desc:header file containing functions declaration
-*/
-
 void print_alphabet(void);
 void jack_bauer(void);
 void print_alphabet_x10(void);
